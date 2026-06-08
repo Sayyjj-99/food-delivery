8e32506cc1e760d326b294e9ac413d0f13e9417b
Sajal <sajaltiwari.wko99@gmail.com>
2026-06-08 11:55:48 +0530

    complete frontend

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -2,39 +2,55 @@ import { createContext, useEffect } from "react";
 import { food_list } from "../assets/assets"
 import { useState } from "react";
 
-export const StoreContext = createContext(null) ;
+export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    
-    const [cartItems , setCartItems] = useState({});
-    
+
+    const [cartItems, setCartItems] = useState({});
+
     const addToCart = (itemID) => {
-        if(!cartItems[itemID]){
-            setCartItems((prev)=>({...prev,[itemID]:1}))
+        if (!cartItems[itemID]) {
+            setCartItems((prev) => ({ ...prev, [itemID]: 1 }))
         }
-        else{
-            setCartItems((prev)=>({...prev,[itemID]:prev[itemID]+1}))
+        else {
+            setCartItems((prev) => ({ ...prev, [itemID]: prev[itemID] + 1 }))
         }
     }
 
     const removeFromCart = (itemID) => {
-        setCartItems((prev)=>({...prev,[itemID]:prev[itemID]-1}))
+        setCartItems((prev) => ({ ...prev, [itemID]: prev[itemID] - 1 }))
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log(cartItems);
     }, [cartItems])
 
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0; // Logic for calculating subTotal
+        for (const item in cartItems) {
+
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+        }
+
+        return totalAmount ;
+    }
+
+
     const contextValue = {
-         food_list,
-         cartItems,
-         setCartItems,
-         addToCart,
-         removeFromCart
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart,
+        getTotalCartAmount
     }
     return (
-        <StoreContext.Provider value = {contextValue}>
+        <StoreContext.Provider value={contextValue}>
             {props.children}
         </StoreContext.Provider>
     )
